b4f950b05b19b6d6ea196cc003827f4a03807e12
Saira706 <155745161+sairabanuu@users.noreply.github.com>
2025-11-17 11:07:31 +0530

    Add files via upload

---
+++ file handing13.py
@@ -0,0 +1,16 @@
+import json
+json.dump('data','file'),
+json.load()
+json.dump()
+json.dumps()
+json.loads()
+# os module
+import os
+os.getlogin()
+os.cpu_count()
+os.system()
+os.getcwd()
+os.chdir()
+os.listdir()
+os.mkdir()
+os.makedirs
